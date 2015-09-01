2dc21a51cc543e1940c934d971ee77355147c969
Steve <steve@tripgrid.com>
2015-09-01 16:48:03 -0700

    add styling for lists on classifieds index page

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -4,7 +4,7 @@ class Classified < ActiveRecord::Base
   belongs_to :User
   #has_many :questions
   #has_many :answers
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", mini: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def image_from_url(url)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", mini: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :classifieds
 end
